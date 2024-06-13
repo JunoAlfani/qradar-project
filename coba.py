3bb8b72b375ab1707c017f602f9d042038f467c5
kamikaze <junoalfani@gmail.com>
2024-06-13 13:06:48 +0700

    deleting coba.py file

--- coba.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-from scipy import stats
-import matplotlib.pyplot as plt
-
-x =b['SELLING PRICE']
-y =b['KM DRIVEN']
-slope, intercept, r, p, std_error = stats.linregress(x,y)
